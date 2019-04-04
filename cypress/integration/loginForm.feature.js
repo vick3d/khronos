59c1c276b4466c800ff80afceb7df81dedcdd1dc
Laura Reale <laaru1988@gmail.com>
2019-04-04 09:47:09 +0200

    update login form tests, add kimaiService file

--- cypress/integration/loginForm.feature.js
+++ cypress/integration/loginForm.feature.js
@@ -1,10 +1,17 @@
 describe('Login form', () => {
 	it('when user logs in', () => {
+		const stub = cy.stub()
+		cy.on ('window:alert', stub)
+
     cy.visit('http://localhost:3000')
       .get('button').contains('Get started here!').click()
-      .get('name').type('susan_super')
-      .get('password').type('api_kitten')
-      .get('button').contains('Login').click()
-      .should('contain', 'You are logged in!')
+			.get(':nth-child(1) > .ui > input').type('susan_super')
+			.get(':nth-child(2) > .ui > input').type('api_kitten')
+			.get('button').contains('Login').click()
+			.wait(1000)
+			.get('button').contains('Get started here!').click()
+			.then(() => {
+      	expect(stub.getCall(0)).to.be.calledWith('You are already logged in!')
+    	})
 	})
 })
---
+++ src/kimaiService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const login = async (userName, userPassword) => {
+  const apiUrl = 'https://demo.kimai.org/api/version'
+  let headers = {
+    "X-AUTH-USER":userName,
+    "X-AUTH-TOKEN":userPassword
+	}
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(apiUrl, {
+        headers: headers
+      })
+      .then(response => {
+				resolve(response);
+      });
+  });
+};
