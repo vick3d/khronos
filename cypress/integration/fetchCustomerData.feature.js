2656bd62ee0cf5d843e003a05da1f7c44527fc24
vick3d <viktoro0090@gmail.com>
2019-04-07 21:40:58 +0200

    fix JSON response

--- cypress/integration/fetchCustomerData.feature.js
+++ cypress/integration/fetchCustomerData.feature.js
@@ -44,8 +44,14 @@ describe("User can fetch customer data", () => {
 		});
 
 		cy.get(".customer > .dropdown").click();
-		cy.contains("Borer-Abbott");
-		cy.contains("Block, Dach and Harris");
-		cy.contains("Cronin, Eichmann and Davis");
+		cy.contains("Altenwerth PLC");
+		cy.contains("Beer and Sons");
+		cy.contains("Bernier Group");
+		cy.contains("Borer, Schuster and Hagenes");
+		cy.contains("Conroy LLC");
+		cy.contains("Grady LLC");
+		cy.contains("McClure Inc");
+		cy.contains("Mohr-Auer");
+		cy.contains("Robel, Reynolds and Ryan");
 	});
 });
