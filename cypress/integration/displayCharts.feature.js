6a5d471ac57a90b465d9bb2d1348e3f54b3bed4e
Laura Reale <laaru1988@gmail.com>
2019-04-10 21:31:09 +0200

    fix indentation

--- cypress/integration/displayCharts.feature.js
+++ cypress/integration/displayCharts.feature.js
@@ -1,5 +1,5 @@
 describe('Display charts', () => {
-  beforeEach(function () {
+	beforeEach(function () {
 		cy
 			.get('button').contains('Get started here!').click()
 			.get(':nth-child(1) > .ui > input').type('susan_super')
@@ -7,12 +7,12 @@ describe('Display charts', () => {
 			.get('button').contains('Login').click()
 	})
 
-  it('User can see saved times', () => {
-	cy
-	.get('.two > :nth-child(2) > .white').click()
-	.get('.info-box-number-l').contains('Working Hours (Today)')
-	.get('.info-box-number-r').contains('Revenue (Today)')
-	.get(':nth-child(1) > .chartjs-render-monitor')
-	.get(':nth-child(2) > .chartjs-render-monitor')
-  })
+	it('User can see saved times', () => {
+		cy
+			.get('.two > :nth-child(2) > .white').click()
+			.get('.info-box-number-l').contains('Working Hours (Today)')
+			.get('.info-box-number-r').contains('Revenue (Today)')
+			.get(':nth-child(1) > .chartjs-render-monitor')
+			.get(':nth-child(2) > .chartjs-render-monitor')
+	})
 })
