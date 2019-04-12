f7ab0053681771b3b51faf5a4cd7ea4f0a92b241
Per Eriksson <per_hegg@hotmail.com>
2019-04-12 12:02:06 +0200

    moves play and stop icon to only show on timetrackingtable

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,7 +25,7 @@ class Navbar extends Component {
 	}
 
 	render() {
-		if (this.props.isLoggedIn) {
+		if (this.props.renderedTimes) {
 			return (
 				<Menu>
 					<Menu.Item link name='logo' onClick={ () => this.props.dashboard()} >
@@ -47,19 +47,35 @@ class Navbar extends Component {
 						<Icon link inverted color='white' name='calendar alternate outline' size='big' />
 					</Menu.Item>
 				</Menu>)
+		} else if (this.props.isLoggedIn) {
+				return (
+					<Menu>
+						<Menu.Item link name='logo' onClick={ () => this.props.dashboard()} >
+							<Image src={logo} id='logo' size='small' />
+						</Menu.Item>
+						<Menu.Item >
+							<Icon link inverted color='white' name='sign-out' size='big' onClick={() => this.logout()} />
+						</Menu.Item>
+						<Message background='green' size='big'>{this.props.message}</Message>
+						<Menu.Item position='right'>
+							<Icon link inverted color='white' name='help' size='big' />
+							<Divider vertical />
+							<Icon link inverted color='white' name='calendar alternate outline' size='big' />
+						</Menu.Item>
+					</Menu>)
 		} else {
-			return (
-				<Menu>
-					<Menu.Item link name='logo'>
-						<Image src={logo} id='logo' size='small' />
-					</Menu.Item>
-					<Menu.Item >
-						<Icon link inverted color='white' name='sign-in' size='big' onClick={ () => this.props.renderLoginForm()} />
-					</Menu.Item>
-					<Menu.Item position='right'>
-						<Icon link inverted color='white' name='help' size='big' />
-					</Menu.Item>
-				</Menu>
+				return (
+					<Menu>
+						<Menu.Item link name='logo'>
+							<Image src={logo} id='logo' size='small' />
+						</Menu.Item>
+						<Menu.Item >
+							<Icon link inverted color='white' name='sign-in' size='big' onClick={ () => this.props.renderLoginForm()} />
+						</Menu.Item>
+						<Menu.Item position='right'>
+							<Icon link inverted color='white' name='help' size='big' />
+						</Menu.Item>
+					</Menu>
 			)
 		}
 	}
